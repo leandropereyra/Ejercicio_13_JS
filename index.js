ae40a0901959d61a39901ed6cf24cc083699a6cc
Gastón Leandro Pereyra <leandro-pereyra@hotmail.es>
2022-06-14 15:24:24 +0200

    Ejercicio -13 - JS

---
+++ index.js
@@ -0,0 +1,35 @@
+//Antes:
+// const nombre = "Leandro";
+// const apellido = "Pereyra";
+// const estudiante = nombre.concat(" ").concat(apellido);
+
+// const estudianteMayuscula = estudiante.toUpperCase();
+// const estudianteMinuscula = estudiante.toLowerCase();
+
+// const estudianteLongitud = estudiante.length;
+
+// const nombreInicial = nombre.substring(0, 1);
+
+// const apellidoFinal = apellido.substring(apellido.length - 1, apellido.length);
+
+// const estudianteSinEspacios = estudiante.replace(/ /g, ``);
+
+// const nombreEnEstudiante = estudiante.includes(nombre);
+
+//Luego:
+const nombre = "Leandro"
+const apellido = "Pereyra"
+const estudiante = nombre.concat(" ").concat(apellido)
+
+const estudianteMayuscula = estudiante.toUpperCase()
+const estudianteMinuscula = estudiante.toLowerCase()
+
+const estudianteLongitud = estudiante.length
+
+const nombreInicial = nombre.substring(0, 1)
+
+const apellidoFinal = apellido.substring(apellido.length - 1, apellido.length)
+
+const estudianteSinEspacios = estudiante.replace(/ /g, "")
+
+const nombreEnEstudiante = estudiante.includes(nombre)
